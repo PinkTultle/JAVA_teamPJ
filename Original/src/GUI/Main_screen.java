e45666bdac58dc273b942911848a0cb8a01795d1
PinkTultle <inseuk1007@naver.com>
2023-10-15 17:43:21 +0900

    메인 프레임 작업중 - 1 배너 패널 삽입 - 배너 패널 크기 조정후 기능 영역 패널 삽입 구현 예정 이후 패널 교체 구현 필요

---
+++ Original/src/GUI/Main_screen.java
@@ -0,0 +1,86 @@
+package GUI;
+
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.ActiveEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+public class Main_screen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Mainframe MF = new Mainframe();
+
+	}
+
+}
+
+
+class Mainframe extends JFrame{
+	
+	private JPanel Operating_Area = null;
+	
+	public Mainframe() {
+		
+		setSize(900, 600);
+		setLocationRelativeTo(null);
+		setTitle("메인 프레임");
+		
+		Banner Mb = new Banner("홈 화면");
+		
+		//기본 패널 삽입
+		//홈 패널 불러와서 삽입
+		
+		add(Mb);
+		
+		setResizable(false);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		setVisible(true);
+
+
+	}
+	
+}
+
+
+
+class Banner extends JPanel{
+	
+	private JLabel Title = null;
+	
+	public Banner(String S) {
+		
+		setSize(new Dimension(500,200));
+		setBackground(Color.GRAY);
+		
+		Title = new JLabel();
+		
+		
+		Font f = new Font("Gothic", Font.ITALIC, 60);
+		Title.setLocation(5, 5);
+		Title.setFont(f);
+		
+		
+		
+		change_Title(S);
+		
+		add(Title);
+	
+	}
+	
+	void change_Title(String S) {
+		Title.setText(S);
+	}
+	
+	
+}
+
