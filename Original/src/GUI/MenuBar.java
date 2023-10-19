1d7dcdc4908755cfd1d36a423f373a1f82e8b9fa
PinkTultle <inseuk1007@naver.com>
2023-10-19 21:36:31 +0900

    메뉴바 구현 이벤트 처리 필요

--- Original/src/GUI/MenuBar.java
+++ Original/src/GUI/MenuBar.java
@@ -1,33 +1,48 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
-public class MenuBar extends JPanel {
+public class MenuBar extends JPanel implements ActionListener{
 	
 	private JButton MenuButton;
 	private String [] tagList = {"홈", "목록", "신청관리", "마이페이지"};
-	private int Bnum = 4;
+	private int length = 520, Bnum = (length - 10) / 4;
 	
-	public static void main(String[] args) {
-		MenuBar M = new MenuBar();
-	}
 	
 	public MenuBar() {
 				
-		setBounds(530, 120, 520, 30);
+		setBounds(530, 120, length, 30);
 		setBackground(Color.white);
 		
+		//버튼 왼쪽부터 정렬 및 버튼 사이 여백 없에기
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		
-		setLayout(new FlowLayout());
-		
+		//버튼 1부터 4까지 설정
 		for(int i = 0 ; i < 4; i++) {
 			MenuButton = new JButton(tagList[i]);
-			MenuButton.setSize(530 / Bnum, 120);
+			MenuButton.setPreferredSize(new Dimension(Bnum, 30));
+			MenuButton.setBackground(Color.WHITE);
+			MenuButton.setHorizontalAlignment(SwingConstants.CENTER);
+			MenuButton.setVerticalAlignment(SwingConstants.CENTER);
 			add(MenuButton);
 		}
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}		
 }
