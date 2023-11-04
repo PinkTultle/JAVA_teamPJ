a6773bdf14e15b68442941ac45623e3015533e64
KJH_ <kjhkmn80@gmail.com>
2023-11-04 19:25:58 +0900

    신고 접수/내역 패널 추가 ( 서로 버튼클릭 이벤트로 호출하는 부분은 구현못함 )

---
+++ Original/src/GUI/Non_Editable_Table.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
+
+// 신고 접수/내역 패널에서 벡터로 만든 테이블의 내용을 사용자가 임의로 수정하지 못하게 하는
+// 테이블 내용 수정 불가 클래스
+class NonEditableTableModel extends DefaultTableModel {
+    
+    NonEditableTableModel(Vector<Vector<Object>> data, Vector<String> columnNames) {
+        super(data, columnNames);
+    }
+    
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        // 모든 셀을 수정할 수 없게 함
+        return false;
+    }
+}
