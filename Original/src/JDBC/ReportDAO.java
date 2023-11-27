cb6a83aec8c257344d67f21163ea3f50321c004c
DESKTOP-0IMQOOQ\dot01 <dot0112dot0112@gmail.com>
2023-11-27 13:03:19 +0900

    신고 내역 연결

---
+++ Original/src/JDBC/ReportDAO.java
@@ -0,0 +1,65 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class ReportDAO {
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+
+	String user = "ABC"; // db 사용자 이름
+	String password = "1234"; // db
+
+	public Connection getConn() throws ClassNotFoundException {
+		Connection conn = null;
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection(url, user, password);
+
+			// System.out.println("로그인 성공");
+
+		} catch (SQLException e) {
+			System.out.println("DB 로그인 실패");
+		}
+		return conn; // 오라클 로그인 연결 정보
+	}
+
+	public Vector<ReportDTO> allReportData() {
+		Vector<ReportDTO> list = new Vector<ReportDTO>();
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null; // 결과 담는 곳
+
+		String sql = "SELECT * FROM 신고기록";
+
+		try {
+			con = getConn();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				ReportDTO dto = new ReportDTO();
+				dto.setReportNum(rs.getInt("신고번호"));
+				dto.setItemNumber(rs.getInt("물품코드"));
+				dto.setItemName(rs.getString("물품명"));
+				dto.setCategory(rs.getString("신고분류"));
+				dto.setStatus(rs.getString("처리상태"));
+				dto.setReportDetail(rs.getString("신고메세지"));
+
+				list.add(dto);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+}
