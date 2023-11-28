802d4a6b13ea50af632a01d332e78765f911b21b
PinkTultle <inseuk1007@naver.com>
2023-11-29 00:21:14 +0900

    데이터베이스 주소 추가

--- Original/src/JDBC/UserDAO.java
+++ Original/src/JDBC/UserDAO.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 public class UserDAO implements AutoCloseable { // 회원 관련 db 기능 
 	
 	//String url = "jdbc:oracle:thin:@192.168.124.100:1521:xe";
-	String url = "jdbc:oracle:thin:@localhost:1521:xe"; //안되면 이걸로!
+	//String url = "jdbc:oracle:thin:@localhost:1521:xe"; //안되면 이걸로!
+	String url = "jdbc:oracle:thin:@115.140.208.29:1521:xe"; //안되면 이걸로!
 	
 	String user = "ABC";       // db 사용자 이름
 	String password = "1234";  // db 
