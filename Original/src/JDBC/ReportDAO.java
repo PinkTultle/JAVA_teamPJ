d717dc6db2cdd5789d6bc4c34bfd4f7f4efdb85d
Seungeun Han <ew0297@gmail.com>
2023-11-29 15:32:01 +0900

    데이터베이스 주소 추가

--- Original/src/JDBC/ReportDAO.java
+++ Original/src/JDBC/ReportDAO.java
@@ -8,7 +8,9 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 public class ReportDAO {
-	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String url = "jdbc:oracle:thin:@115.140.208.29:1521:xe";
+
+	//String url = "jdbc:oracle:thin:@localhost:1521:xe";
 
 	String user = "ABC"; // db 사용자 이름
 	String password = "1234"; // db
