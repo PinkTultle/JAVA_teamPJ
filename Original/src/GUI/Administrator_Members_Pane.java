81f7648947d2b956de28ced8b19a03a30d5171f3
PinkTultle <inseuk1007@naver.com>
2023-12-12 19:03:29 +0900

    no message

--- Original/src/GUI/Administrator_Members_Pane.java
+++ Original/src/GUI/Administrator_Members_Pane.java
@@ -42,7 +42,8 @@ public class Administrator_Members_Pane extends Administrator_pane {
 				close_bt.getWidth(), close_bt.getHeight());
 		delete.setBackground(null);
 		add(delete);
-	
+		
+		System.out.print("dada");
 		
 		
 		setBackground(Color.white);
