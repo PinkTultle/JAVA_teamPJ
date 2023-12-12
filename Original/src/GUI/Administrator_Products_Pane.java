bc541865ce56a321e24abdbe992209d86abfad7a
DESKTOP-UG2A50B\User <dot0112dot0112@gmail.com>
2023-12-12 22:14:23 +0900

    물품 정보 연결

--- Original/src/GUI/Administrator_Products_Pane.java
+++ Original/src/GUI/Administrator_Products_Pane.java
@@ -8,71 +8,65 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
 import JDBC.ItemDAO;
-import JDBC.UserDAO;
 
-public class Administrator_Products_Pane extends Administrator_pane  {
-	
+public class Administrator_Products_Pane extends Administrator_pane {
+
 	private JButton revise, delete;
-	private String colNames[] = {"물품코드","카테고리","물품명","소유주", "대여상태"};  // 테이블 컬럼 값들
+	private String colNames[] = { "물품코드", "카테고리", "물품명", "소유주", "대여상태" }; // 테이블 컬럼 값들
 
-    private ItemDAO dao;
-	
-	public Administrator_Products_Pane(JFrame master){
+	private ItemDAO dao;
+
+	public Administrator_Products_Pane(JFrame master) {
 		super(master);
-		
+
 		Refresh_table();
 		set_table();
-		
-		
+
 		scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(18, 30, 460, 344);
 		scrollPane.setBackground(Color.white);
 		add(scrollPane);
 
-		Action act =  new Action();
+		Action act = new Action();
 
 		revise = new JButton("수정");
-		revise.setBounds(close_bt.getX(), scrollPane.getY(),
-					close_bt.getWidth(), close_bt.getHeight());
+		revise.setBounds(close_bt.getX(), scrollPane.getY(), close_bt.getWidth(), close_bt.getHeight());
 		revise.setBackground(null);
 		revise.addActionListener(act);
 		add(revise);
 
 		delete = new JButton("삭제");
-		delete.setBounds(close_bt.getX(), revise.getY()+revise.getHeight()+20,
-				close_bt.getWidth(), close_bt.getHeight());
+		delete.setBounds(close_bt.getX(), revise.getY() + revise.getHeight() + 20, close_bt.getWidth(),
+				close_bt.getHeight());
 		delete.setBackground(null);
 		delete.addActionListener(act);
 		add(delete);
-		
-		
-		
+
 		setBackground(Color.white);
-		
+
 	}
-	
+
 	private void Refresh_table() {
-		
+
 		model = new DefaultTableModel(colNames, 0) {
-			 public boolean isCellEditable(int row, int column) {
-			    	if(column >= 0) {
-			    		return false;
-			    	}else {
-			    		return true;
-			    	}
-			    }
+			public boolean isCellEditable(int row, int column) {
+				if (column >= 0) {
+					return false;
+				} else {
+					return true;
+				}
+			}
 		};
-		
+
 		table = new JTable(model);
-		
-		table.getColumn("물품코드").setPreferredWidth(30); 
-		table.getColumn("대여상태").setPreferredWidth(40); 
-		table.getColumn("소유주").setPreferredWidth(50);		
-		
+
+		table.getColumn("물품코드").setPreferredWidth(30);
+		table.getColumn("대여상태").setPreferredWidth(40);
+		table.getColumn("소유주").setPreferredWidth(50);
+
 		dao = new ItemDAO();
 		try {
 			dao.itemAll(model);
@@ -80,22 +74,23 @@ public class Administrator_Products_Pane extends Administrator_pane  {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	class Action implements ActionListener{
-		
+
+	class Action implements ActionListener {
+
 		@Override
-		public void actionPerformed(ActionEvent e) {			
-			
-			if(e.getSource() == delete) {
+		public void actionPerformed(ActionEvent e) {
+
+			if (e.getSource() == delete) {
 				dao.deleteItem(Integer.parseInt(table.getValueAt(table.getSelectedRow(), 0).toString()));
-				
+
 			}
-			if(e.getSource() == revise) {
-				//물품 프로필 오픈
+			if (e.getSource() == revise) {
+				new Administrator_ItemInfo_frame(
+						Integer.parseInt(table.getValueAt(table.getSelectedRow(), 0).toString()));
 			}
-			
+
 		}
 	}
 
